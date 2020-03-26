74d62e5f29646a96b9eb0813bf005adf18f2212d
TimHertsens930 <tim.hertsens@gmail.com>
2020-03-27 00:17:36 +0100

    updates voor uart comms
    extra pin header file aangemaakt voor werken met het shield, IE knoppen vervangen voor PP regeling, hold knop vervangen voor IE regeling (werkt nog niet op LCD), alle serial naar serial1, IE wordt nu verstuurd als float, en nog wat details.

--- arduino_slave_code.ino
+++ arduino_slave_code.ino
@@ -1,9 +1,8 @@
-//#include <Arduino.h>
-//#include <HardwareSerial.h>
-//#include <Print.h>
+
 #include <Wire.h>
 #include "LiquidCrystal_I2C.h"
-#include "pins.h"
+//#include "pins.h" //wiring volgens pcb ontwerp
+#include "pinsShield.h" //wegens andere wiring met voorlopig shield voor demo
 
 
 //Constants Tim
@@ -20,17 +19,18 @@ char receivedChars1[numChars];
 boolean newData0 = false;
 boolean newData1 = false;
 
-
+int sendDelay = 100;
 
 //Constants Hans
 
 // define bytes to send
-unsigned int RR = 0;    // Number of breaths per minute setting
-unsigned int VT = 0;    // Tidal volume= target to deliver
+unsigned int RR = 20;    // Number of breaths per minute setting
+unsigned int VT = 600;    // Tidal volume= target to deliver
 unsigned int PK = 50;   //Peak pressure
 unsigned int TS = 0;    // Breath Trigger Sensitivity = amount the machine should look for
 unsigned int IE = 1;           // Inspiration-expiration rate
-// unsigned int PP = 0;    // PEEP Pressure = Max pressure to deliver --> Manueel instellen op peep valve
+unsigned int PP = 0;    // PEEP Pressure = Max pressure to deliver --> Manueel instellen op peep valve
+//waarde moet wel gekend zijn in het systeem (en moet dus instelbaar zijn)
 
 unsigned int ADPK = 10; // Allowed Deviation Peak pressure
 unsigned int ADVT = 10; // Allowed Deviation Tidal Volume
@@ -116,24 +116,24 @@ bool alarm = false;
 
 void setup() {
 
-  Serial.begin(115200);
-  Serial.println("Init LCD");
+  Serial1.begin(115200);
+  //Serial1.println("Init LCD");
   //init LCD
   lcd.backlight();
   lcd.init();
   //print values void
-  Serial.println("Print letters");
+  Serial1.println("Print letters");
   printLetters();
 
-  Serial.println("Wacht op OK");
+  Serial1.println("Wacht op OK");
   while (newData1 == 0 ) {
     recvWithEndMarkerSer1(); //keep looking
     if (newData1 == true) {
       //processSerialPort(receivedChars1);
       if (receivedChars1[0] == 'O' && receivedChars1[1] == 'K') {
-        //        Serial.println("Joepie!");
+        //        Serial1.println("Joepie!");
       } else {
-        Serial.print(receivedChars1);
+        Serial1.print(receivedChars1);
         newData1 = false;
       }
 
@@ -201,12 +201,12 @@ void loop() {
         newData0 = false;
         if (alarm) { //als het alarm actief was
           alarm = false;
-          Serial.print("Back Alive.");
+          //Serial.print("Back Alive.");
           al = 0;
         }
         else // als het alarm niet actief was
         {
-          Serial.print("Still Alive.");
+          //Serial.print("Still Alive.");
           alarm = false;
           al = 0;
         }
@@ -222,7 +222,7 @@ void loop() {
 
 
   if (alarm) {
-    alarmke();
+    alarmInitiated();
   }
 
   delay(100);
@@ -234,14 +234,14 @@ void loop() {
     alarm = true;
   }
 
-  if (k > 9) {
+  /*if (k > 9) {
     //dump all data
     for (int i = 0; i < numButtons; i++) {
       buttons[i] = 1;
     }
     serialSend();
     k = 0;
-  }
+  }*/ //efkes niet elke seconde data dumpen
 
 
 }
@@ -278,9 +278,9 @@ void buttonsRead() {
   /*
     for (int i = 0; i < numButtons; i++)
     {
-    Serial.print(buttonPins[1][i]);
+    Serial1.print(buttonPins[1][i]);
     }
-    Serial.println();
+    Serial1.println();
   */
 
   //invert the numbers
@@ -316,12 +316,16 @@ void buttonsRead() {
   TS = TS +  buttons[POS_TRIG_UP] * jumpValue(POS_TRIG_UP);
   if (TS > 1) {
     TS = TS -  buttons[POS_TRIG_DOWN] * jumpValue(POS_TRIG_DOWN);
-  }//IE
+  }//PP
+  PP = PP +  buttons[POS_IE_UP] * jumpValue(POS_IE_UP);
+  if (PP > 1) {
+    PP = PP -  buttons[POS_IE_DOWN] * jumpValue(POS_IE_DOWN);
+  }  // IE
   if (IE < 3) {
-    IE = IE + buttons[POS_IE_UP];
+    IE = IE + buttons[POS_HOLD];
   }
-  if (IE > 1) {
-    IE = IE - buttons[POS_IE_DOWN];
+  if (IE = 3) {
+    IE = 1;
   }
   // ADPK
 
@@ -367,9 +371,9 @@ int jumpValue(int i) {
   //    return 0;
   // }
   //  lastRequestedValueTime = loopMillis;
-  //Serial.println(loopMillis);
-  //Serial.println(lastDebounceTime[1][i]);
-  // Serial.println(loopMillis-lastDebounceTime[1][0]);
+  //Serial1.println(loopMillis);
+  //Serial1.println(lastDebounceTime[1][i]);
+  // Serial1.println(loopMillis-lastDebounceTime[1][0]);
 
 
 
@@ -392,78 +396,85 @@ void serialSend()
 {
   // RR
   if ((buttons[POS_RR_UP] == 1) || (buttons[POS_RR_DOWN] == 1)) {
-    delay(10);
-    Serial.print("RR=");
-    Serial.println(RR);
+    delay(sendDelay);
+    Serial1.print("RR=");
+    Serial1.println(RR);
   }
   // VT
   if ((buttons[POS_VOLUME_UP] == 1) || (buttons[POS_VOLUME_DOWN] == 1)) {
-    delay(10);
-    Serial.print("VT=");
-    Serial.println(VT);
+    delay(sendDelay);
+    Serial1.print("VT=");
+    Serial1.println(VT);
   }
   // PK
   if ((buttons[POS_PRESSURE_UP] == 1) || (buttons[POS_PRESSURE_DOWN] == 1)) {
-    delay(10);
-    Serial.print("PK=");
-    Serial.println(PK);
+    delay(sendDelay);
+    Serial1.print("PK=");
+    Serial1.println(PK);
   }
   // TS
   if ((buttons[POS_TRIG_UP] == 1) || (buttons[POS_TRIG_DOWN] == 1)) {
-    delay(10);
-    Serial.print("TS=");
-    Serial.println(TS);
+    delay(sendDelay);
+    Serial1.print("TS=");
+    Serial1.println(TS);
   }
-  // IE
+  // IE -->wordt gepruikt voor PP ipv IE, IE wordt veranderd door HOLD.
   if ((buttons[POS_IE_UP] == 1) || (buttons[POS_IE_DOWN] == 1)) {
-    delay(10);
-    Serial.print("IE=");
-    Serial.println(IE);
+    delay(sendDelay);
+    Serial1.print("PP=");
+    Serial1.println(PP);
   }
-  // PP --> kunnen we niet doorsturen
+  // PP --> kunnen we niet doorsturen, nu dus wel weer :)
+
+  // IE op positie HOLD
+  if (buttons[POS_HOLD] == 1) {
+    delay(sendDelay);
+    Serial1.print("IE=");
+    float IEfloat = 1 / IE; //wordt over uart verstuurd als float
+    Serial1.println(IEfloat);
+  }
 
   // ADPK
   if ((buttons[POS_PRESSURE_ALARM_UP] == 1) || (buttons[POS_PRESSURE_ALARM_DOWN] == 1)) {
-    delay(10);
-    Serial.print("ADPK=");
-    Serial.println(ADPK);
+    delay(sendDelay);
+    Serial1.print("ADPK=");
+    Serial1.println(ADPK);
   }
   // ADVT
   if ((buttons[POS_VOLUME_ALARM_UP] == 1) || (buttons[POS_VOLUME_ALARM_DOWN] == 1)) {
-    delay(10);
-    Serial.print("ADVT=");
-    Serial.println(ADVT);
+    delay(sendDelay);
+    Serial1.print("ADVT=");
+    Serial1.println(ADVT);
   }
   // ADPP
   if ((buttons[POS_PEEP_ALARM_UP] == 1) || (buttons[POS_PEEP_ALARM_DOWN] == 1)) {
-    delay(10);
-    Serial.print("ADPP=");
-    Serial.println(ADPP);
+    delay(sendDelay);
+    Serial1.print("ADPP=");
+    Serial1.println(ADPP);
   }
 
   // MODE
   if (buttons[POS_MODE] == 1) {
-    Serial.print("MODE=");
-    Serial.println(MODE);
+    delay(sendDelay); //delay is nodig voor inlezen, aanvraag van thomas
+    Serial1.print("MODE=");
+    Serial1.println(MODE);
   }
 
   // MUTE
   if (buttons[POS_MUTE] == 1) {
-    Serial.print("MUTE=");
-    Serial.println(MUTE);
+    delay(sendDelay); //delay is nodig voor inlezen, aanvraag van thomas
+    Serial1.print("MUTE=");
+    Serial1.println(MUTE);
   }
 
   // ACTIVE
   if (buttons[POS_START_STOP] == 1) {
-    Serial.print("ACTIVE=");
-    Serial.println(ACTIVE);
+    delay(sendDelay); //delay is nodig voor inlezen, aanvraag van thomas
+    Serial1.print("ACTIVE=");
+    Serial1.println(ACTIVE);
   }
-  // TODO: hold
-}
 
-/*void serialSendSpecific(){
-  Serial.print();
-  }*/
+}
 
 void printValues() {
   lcd.setCursor(12, 2);
@@ -481,8 +492,8 @@ void printValues() {
   lcd.print(RR);
   lcd.setCursor(15, 1);
   lcd.print(TS);
-  //  lcd.setCursor(3, 2);
-  //  lcd.print(PP);
+  lcd.setCursor(3, 2);
+  lcd.print(PP);
   lcd.setCursor(7, 2);
   lcd.print(ADPP);
   if (MUTE) {
@@ -508,8 +519,8 @@ void printLetters()
   lcd.print("RR");
   lcd.setCursor(12, 1);
   lcd.print("TS");
-  //  lcd.setCursor(0, 2);
-  //  lcd.print("PP");
+  lcd.setCursor(0, 2);
+  lcd.print("PP");
   lcd.setCursor(12, 3);
   lcd.print("IE");
   lcd.setCursor(15, 3);
@@ -541,14 +552,14 @@ void clearValues()
 }
 
 void recvWithEndMarkerSer1() {
-  //this waits for confirmation over serial that the data is OK
+  //this waits for confirmation over Serial1 that the data is OK
 
   static byte ndx = 0;
   char endMarker = '\n';
   char rc;
   //if there is still data in the tube
-  while (Serial.available() > 0 && newData1 == false) {
-    rc = Serial.read(); //keep reading
+  while (Serial1.available() > 0 && newData1 == false) {
+    rc = Serial1.read(); //keep reading
     if (rc != endMarker) { //if there is no endmarker detected
       receivedChars1[ndx] = rc; //put chars in array
       ndx++; //make array larger
@@ -559,21 +570,21 @@ void recvWithEndMarkerSer1() {
       receivedChars1[ndx] = '\0'; // terminate the string
       ndx = 0;
       newData1 = true;
-      Serial.println(receivedChars1);
+      Serial1.println(receivedChars1);
     }
   }
 }
 
 
 void recvWithEndMarkerSer0() {
-  //this waits for confirmation over serial that the data is OK
+  //this waits for confirmation over Serial1 that the data is OK
 
   static byte ndx = 0;
   char endMarker = '\n';
   char rc;
   //if there is still data in the tube
-  while (Serial.available() > 0 && newData0 == false) {
-    rc = Serial.read(); //keep reading
+  while (Serial1.available() > 0 && newData0 == false) {
+    rc = Serial1.read(); //keep reading
     if (rc != endMarker) { //if there is no endmarker detected
       receivedChars0[ndx] = rc; //put chars in array
       ndx++; //make array larger
@@ -584,12 +595,12 @@ void recvWithEndMarkerSer0() {
       receivedChars0[ndx] = '\0'; // terminate the string
       ndx = 0;
       newData0 = true;
-      Serial.println(receivedChars0);
+      Serial1.println(receivedChars0);
     }
   }
 }
 
-void alarmke()
+void alarmInitiated()
 {
   /*
     # 1    (1 << 0) Mechanical failure
@@ -607,14 +618,14 @@ void alarmke()
   {
     //lcd flikkert
 
-    Serial.println("ALARM!");
+    Serial1.println("ALARM!");
     lcd.setCursor(1, 3);
     lcd.print("ALARM!");
     //"alarm" komt op LCD
     //type alarm komt op LCD
     buttonsRead();
   }
-  Serial.println("ALARM MUTED");
+  Serial1.println("ALARM MUTED");
   lcd.setCursor(1, 3);
   lcd.print("       ");
 
