fc3c582fda434a8a8e359a844d6bb260e2b2ec7b
Hans <hans_merken@hotmail.com>
2020-03-26 09:56:58 +0100

    Updates van Tim

--- VUB_Buttons.ino
+++ VUB_Buttons.ino
@@ -5,38 +5,44 @@
 #include "LiquidCrystal_I2C.h"
 #include "pins.h"
 
+
+
 //Constants Tim
 
 
 LiquidCrystal_I2C lcd(0x3f, 20,  4);
 
 
+// Communication with Main
+String value0;
+String value1;
+const byte numChars = 32;
+char receivedChars0[numChars];
+char receivedChars1[numChars];
+boolean newData0 = false;
+boolean newData1 = false;
+
 
 
 //Constants Hans
 
 // define bytes to send
-unsigned int RR = 0;
-// Number of breaths per minute setting
-unsigned int VT = 0;
-// Tidal volume= target to deliver
-unsigned int PK = 50;
-//Peak pressure
-unsigned int TS = 0;
-// Breath Trigger Sensitivity = amount the machine should look for
-float IE = 0;
-// Inspiration-expiration rate
-unsigned int PP = 0;
-//PEEP Pressure = Max pressure to deliver
-bool MO = false; //false = Pressure
-//Mode
+unsigned int RR = 0;    // Number of breaths per minute setting
+unsigned int VT = 0;    // Tidal volume= target to deliver
+unsigned int PK = 50;   //Peak pressure
+unsigned int TS = 0;    // Breath Trigger Sensitivity = amount the machine should look for
+float IE = 0;           // Inspiration-expiration rate
+// unsigned int PP = 0;    // PEEP Pressure = Max pressure to deliver --> Manueel instellen op peep valve
 
-unsigned int PKtresh = 10;
-unsigned int VTtresh = 10;
-unsigned int PPtresh = 5;
+unsigned int ADPK = 10; // Allowed Deviation Peak pressure
+unsigned int ADVT = 10; // Allowed Deviation Tidal Volume
+unsigned int ADPP = 5;  // Allowed Deviation PEEP
 
+bool MODE = false; // Mode (false = Pressure)
+bool MUTE = false; // Mute alarms (true = mute alarms)
+bool ACTIVE = false; // Start/stop (true = start)
 
-
+bool flag = false;
 
 
 // constants won't change. They're used here to set pin numbers:
@@ -49,26 +55,58 @@ unsigned long debounceDelay = 50; // the debounce time; increase if the output f
 uint32_t lastRequestedValueTime = 0;
 uint32_t loopMillis;
 
-const int numButtons = 11;
-unsigned long lastDebounceTime[2][numButtons] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; // the last time the output pin was toggled
+const int numButtons = 20;
+unsigned long lastDebounceTime[2][numButtons] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; // the last time the output pin was toggled
 //make an array with all button pins and last button state
 int buttonPins[2][numButtons] = { {
-    BUTTON_VOLUMEPRESSURE_UP,
-    BUTTON_VOLUMEPRESSURE_DOWN,
-    BUTTON_VOLUME_UP,
     BUTTON_VOLUME_DOWN,
-    BUTTON_BPM_UP,
-    BUTTON_BPM_DOWN,
-    BUTTON_TRIG_UP,
+    BUTTON_VOLUME_UP,
+    BUTTON_PRESSURE_DOWN,
+    BUTTON_PRESSURE_UP,
     BUTTON_TRIG_DOWN,
+    BUTTON_TRIG_UP,
+    BUTTON_RR_DOWN,
+    BUTTON_RR_UP,
+    BUTTON_START_STOP,
     BUTTON_MUTE,
-    BUTTON_BREATH,
-    BUTTON_MODE
+    BUTTON_HOLD,
+    BUTTON_MODE,
+    BUTTON_PRESSURE_ALARM_DOWN,
+    BUTTON_PRESSURE_ALARM_UP,
+    BUTTON_VOLUME_ALARM_DOWN,
+    BUTTON_VOLUME_ALARM_UP,
+    BUTTON_PEEP_ALARM_DOWN,
+    BUTTON_PEEP_ALARM_UP,
+    BUTTON_IE_DOWN,
+    BUTTON_IE_UP
   }, {
-    HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH
+    HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH
   }
 };
-int buttons [numButtons] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int buttons [numButtons] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+// Constants Loes
+#define POS_VOLUME_DOWN     0
+#define POS_VOLUME_UP       1
+#define POS_PRESSURE_DOWN   2
+#define POS_PRESSURE_UP     3
+#define POS_TRIG_DOWN       4
+#define POS_TRIG_UP         5
+#define POS_RR_DOWN        6
+#define POS_RR_UP          7
+#define POS_START_STOP      8
+#define POS_MUTE            9
+#define POS_HOLD            10
+#define POS_MODE            11
+#define POS_PRESSURE_ALARM_DOWN 12
+#define POS_PRESSURE_ALARM_UP   13
+#define POS_VOLUME_ALARM_DOWN   14
+#define POS_VOLUME_ALARM_UP     15
+#define POS_PEEP_ALARM_DOWN     16
+#define POS_PEEP_ALARM_UP       17
+#define POS_IE_DOWN             18
+#define POS_IE_UP               19
 
 
 bool test = false;
@@ -76,39 +114,60 @@ bool test = false;
 void setup() {
   //for debugging
   Serial.begin(115200);
+   Serial.println("Init LCD");
   //init LCD
   lcd.backlight();
   lcd.init();
   //print values void
+   Serial.println("Print letters");
   printLetters();
+
+ /*Serial.println("Wacht op OK");
+  while(newData1 == 0 ){
+      recvWithEndMarkerSer1();
+      if (newData1 == true){
+        //processSerialPort(receivedChars1);
+        if (receivedChars1 == 'OK'){
+          Serial.println("Joepie!");
+        } else {
+          newData1 = false;
+        }
+        
+      }
+      for (int i = 0; i < numButtons; i++) {
+        buttons[i]=1;
+      }
+      serialSend();
+  }
+*/
+
+
+  
   //declare all buttons
-
-  pinMode(BUTTON_MODE, INPUT_PULLUP);
-
-  pinMode(BUTTON_VOLUMEPRESSURE_UP, INPUT_PULLUP);
-  pinMode(BUTTON_VOLUMEPRESSURE_DOWN, INPUT_PULLUP);
-
-  pinMode(BUTTON_VOLUME_UP, INPUT_PULLUP);
   pinMode(BUTTON_VOLUME_DOWN, INPUT_PULLUP);
-
-  pinMode(BUTTON_BPM_UP, INPUT_PULLUP);
-  pinMode(BUTTON_BPM_DOWN, INPUT_PULLUP);
-
-  pinMode(BUTTON_TRIG_UP, INPUT_PULLUP);
+  pinMode(BUTTON_VOLUME_UP, INPUT_PULLUP);
+  pinMode(BUTTON_PRESSURE_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_PRESSURE_UP, INPUT_PULLUP);
   pinMode(BUTTON_TRIG_DOWN, INPUT_PULLUP);
-
+  pinMode(BUTTON_TRIG_UP, INPUT_PULLUP);
+  pinMode(BUTTON_RR_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_RR_UP, INPUT_PULLUP);
   pinMode(BUTTON_START_STOP, INPUT_PULLUP);
-
-  pinMode(BUTTON_BREATH, INPUT_PULLUP);
-
   pinMode(BUTTON_MUTE, INPUT_PULLUP);
-
-  pinMode(ALARMPIN, INPUT_PULLUP);
-
+  pinMode(BUTTON_HOLD, INPUT_PULLUP);
+  pinMode(BUTTON_MODE, INPUT_PULLUP);
+  pinMode(BUTTON_PRESSURE_ALARM_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_PRESSURE_ALARM_UP, INPUT_PULLUP);
+  pinMode(BUTTON_VOLUME_ALARM_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_VOLUME_ALARM_UP, INPUT_PULLUP);
+  pinMode(BUTTON_PEEP_ALARM_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_PEEP_ALARM_UP, INPUT_PULLUP);
+  pinMode(BUTTON_IE_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_IE_UP, INPUT_PULLUP);
 }
 
 void loop() {
-  
+
   loopMillis = millis();
   //read button inputs
   buttonsRead();
@@ -117,9 +176,13 @@ void loop() {
   //edit parameters
 
   //send parameters
-  serialSend();
+  if (flag == true) {
+    serialSend();
+    flag = false;
+  }
+  delay(10);
+  
   //Update screen
-
   printValues();
 
 
@@ -140,7 +203,6 @@ void buttonsRead() {
         buttonPins[1][i] = reading;
         lastDebounceTime[0][i] = millis(); //set the current time
         lastDebounceTime[1][i] = 0; //reset the first time counter for jumpvalue
-
       }
       else if ( reading == LOW && buttonPins[1][i] != LOW) {
         buttonPins[1][i] = reading;
@@ -149,6 +211,7 @@ void buttonsRead() {
         if (lastDebounceTime[1][i] == 0)
         {
           lastDebounceTime[1][i] = millis(); //set the current time to the first time counter for jumpvalue
+          flag = true;
         }
 
       }//close if/else
@@ -166,29 +229,53 @@ void buttonsRead() {
     {
       buttons[j] = 0;
     }
-
   }
+  
   //make the buttons edit the values
-  // PK Pressure 0 & 1
-  PK = PK +  buttons[0] * jumpValue(0);
-  PK = PK -  buttons[1] * jumpValue(1);
-  // VT 2 & 3
-  VT = VT +  buttons[2] * jumpValue(2);
-  VT = VT -  buttons[3] * jumpValue(3);
-  //RR 4 & 5
-  RR = RR +  buttons[4] * jumpValue(4);
-  RR = RR -  buttons[5] * jumpValue(5);
-  //TS 6 & 7
-  TS = TS +  buttons[6] * jumpValue(6);
-  TS = TS -  buttons[7] * jumpValue(7);
+  //RR
+  RR = RR +  buttons[POS_RR_UP] * jumpValue(POS_RR_UP);
+  RR = RR -  buttons[POS_RR_DOWN] * jumpValue(POS_RR_DOWN);
+  // VT
+  VT = VT +  buttons[POS_VOLUME_UP] * jumpValue(POS_VOLUME_UP);
+  VT = VT -  buttons[POS_VOLUME_DOWN] * jumpValue(POS_VOLUME_DOWN);
+  // PK
+  PK = PK +  buttons[POS_PRESSURE_UP] * jumpValue(POS_PRESSURE_UP);
+  PK = PK -  buttons[POS_PRESSURE_DOWN] * jumpValue(POS_PRESSURE_DOWN);
+  //TS
+  TS = TS +  buttons[POS_TRIG_UP] * jumpValue(POS_TRIG_UP);
+  TS = TS -  buttons[POS_TRIG_DOWN] * jumpValue(POS_TRIG_DOWN);
+  //IE
+  IE = IE + 0.1 * buttons[POS_IE_UP];
+  IE = IE - 0.1 * buttons[POS_IE_DOWN];
 
+  // ADPK
+  ADPK = ADPK +  buttons[POS_PRESSURE_ALARM_UP] * jumpValue(POS_PRESSURE_ALARM_UP);
+  ADPK = ADPK -  buttons[POS_PRESSURE_ALARM_DOWN] * jumpValue(POS_PRESSURE_ALARM_DOWN);
+  // ADVT
+  ADVT = ADVT +  buttons[POS_VOLUME_ALARM_UP] * jumpValue(POS_VOLUME_ALARM_UP);
+  ADVT = ADVT -  buttons[POS_VOLUME_ALARM_DOWN] * jumpValue(POS_VOLUME_ALARM_DOWN);
+  // ADPP
+  ADPP = ADPP +  buttons[POS_PEEP_ALARM_UP] * jumpValue(POS_PEEP_ALARM_UP);
+  ADPP = ADPP -  buttons[POS_PEEP_ALARM_DOWN] * jumpValue(POS_PEEP_ALARM_DOWN);
+  
   //mode button here
   //if mode is VOLUME and button is pressed: goto PRESSURE
-  if (buttons[10] == 1)
+  if (buttons[POS_MODE] == 1)
   {
-    MO = !MO;
+    MODE = !MODE;
     clearValues();
   }
+
+
+  if (buttons[POS_MUTE] == 1)
+  {
+    MUTE = !MUTE;
+    clearValues();
+  }
+
+
+  // TODO: hold, mute, start/stop
+  
 
 }
 
@@ -222,64 +309,101 @@ int jumpValue(int i) {
 
 void serialSend()
 {
-  //1
-  Serial.print("Mode=");
-  Serial.println(MO);
-  //2
-  Serial.print("PK=");
-  Serial.println(PK);
-  //3
-  Serial.print("VT=");
-  Serial.println(VT);
-  //4
-  Serial.print("RR=");
-  Serial.println(RR);
-  //5
-  Serial.print("TR=");
-  Serial.println(TS);
-  //6
-  Serial.print("IE=");
-  Serial.println(IE);
-  //7
-  Serial.print("PP=");
-  Serial.println(PP);
-  //8
-  Serial.print("PKtresh=");
-  Serial.println(PKtresh);
-  //9
-  Serial.print("VTtresh=");
-  Serial.println(VTtresh);
-  //10
-  Serial.print("PPtresh=");
-  Serial.println(PPtresh);
+  // RR
+  if ((buttons[POS_RR_UP] == 1) || (buttons[POS_RR_DOWN] == 1)) {
+    delay(10);
+    Serial.print("RR=");
+    Serial.println(RR);
+  }
+  // VT
+  if ((buttons[POS_VOLUME_UP] == 1) || (buttons[POS_VOLUME_DOWN] == 1)) {
+    delay(10);
+    Serial.print("VT=");
+    Serial.println(VT);
+  }
+  // PK
+  if ((buttons[POS_PRESSURE_UP] == 1) || (buttons[POS_PRESSURE_DOWN] == 1)) {
+    delay(10);
+    Serial.print("PK=");
+    Serial.println(PK);
+  }
+  // TS
+  if ((buttons[POS_TRIG_UP] == 1) || (buttons[POS_TRIG_DOWN] == 1)) {
+    delay(10);
+    Serial.print("TS=");
+    Serial.println(TS);
+  }
+  // IE
+  if ((buttons[POS_IE_UP] == 1) || (buttons[POS_IE_DOWN] == 1)) {
+    delay(10);
+    Serial.print("IE=");
+    Serial.println(IE);
+  }
+  // PP --> kunnen we niet doorsturen
 
+  // ADPK
+  if ((buttons[POS_PRESSURE_ALARM_UP] == 1) || (buttons[POS_PRESSURE_ALARM_DOWN] == 1)) {
+    delay(10);
+    Serial.print("ADPK=");
+    Serial.println(ADPK);
+  }
+  // ADVT
+  if ((buttons[POS_VOLUME_ALARM_UP] == 1) || (buttons[POS_VOLUME_ALARM_DOWN] == 1)) {
+    delay(10);
+    Serial.print("ADVT=");
+    Serial.println(ADVT);
+  }
+  // ADPP
+  if ((buttons[POS_PEEP_ALARM_UP] == 1) || (buttons[POS_PEEP_ALARM_DOWN] == 1)) {
+    delay(10);
+    Serial.print("ADPP=");
+    Serial.println(ADPP);
+  }
+    
+  // MODE
+  if (buttons[POS_MODE] == 1) {
+    Serial.print("MODE=");
+    Serial.println(MODE);
+  }
 
-
-
-
+  // MUTE
+  if (buttons[POS_MUTE] == 1) {
+    Serial.print("MUTE=");
+    Serial.println(MUTE);
+  }
+  
+  // ACTIVE
+  if (buttons[POS_START_STOP] == 1) {
+    Serial.print("ACTIVE=");
+    Serial.println(ACTIVE);
+  }
+  // TODO: hold
 }
 
+/*void serialSendSpecific(){
+  Serial.print();
+}*/
 
 void printValues() {
   lcd.setCursor(12, 2);
-  if (MO == true) lcd.print("VOLUME");
-  if (MO == false) lcd.print("PRESSURE");
+  if (MODE == true) lcd.print("VOLUME");
+  if (MODE == false) lcd.print("PRESSURE");
   lcd.setCursor(3, 0);
   lcd.print(PK);
   lcd.setCursor(7, 0);
-  lcd.print(PKtresh);
+  lcd.print(ADPK);
   lcd.setCursor(3, 1);
   lcd.print(VT);
   lcd.setCursor(7, 1);
-  lcd.print(VTtresh);
+  lcd.print(ADVT);
   lcd.setCursor(15, 0);
   lcd.print(RR);
   lcd.setCursor(15, 1);
   lcd.print(TS);
-  lcd.setCursor(3, 2);
-  lcd.print(PP);
+//  lcd.setCursor(3, 2);
+//  lcd.print(PP);
   lcd.setCursor(7, 2);
-  lcd.print(PPtresh);
+  lcd.print(ADPP);
   lcd.setCursor(15, 3);
   lcd.print(IE);
 }
@@ -293,8 +417,8 @@ void printLetters()
   lcd.print("RR");
   lcd.setCursor(12, 1);
   lcd.print("TS");
-  lcd.setCursor(0, 2);
-  lcd.print("PP");
+//  lcd.setCursor(0, 2);
+//  lcd.print("PP");
   lcd.setCursor(12, 3);
   lcd.print("IE");
 }
@@ -321,4 +445,26 @@ void clearValues()
   lcd.print("   ");
   lcd.setCursor(15, 3);
   lcd.print("   ");
+}
+
+void recvWithEndMarkerSer1() {
+   static byte ndx = 0;
+   char endMarker = '\n';
+   char rc;
+   while (Serial.available() > 0 && newData1 == false) {
+     rc = Serial.read();
+     if (rc != endMarker) {
+       receivedChars1[ndx] = rc;
+       ndx++;
+       if (ndx >= numChars) {
+         ndx = numChars - 1;
+       }
+     }
+     else {
+       receivedChars1[ndx] = '\0'; // terminate the string
+       ndx = 0;
+       newData1 = true;
+       Serial.println(receivedChars1);
+     }
+   }
 }
--- pins.h
+++ pins.h
@@ -5,28 +5,42 @@
  *      Author: gast1
  */
 
+#define PIC_KEEPALIVE A8
 
+// 6 Light
+// 7 Speaker
+// 8 Graphics - wait
+// 9 Graphics - int
+// 10 Graphics - Backlight
 
-#define BUTTON_MODE 23
+#define BUTTON_VOLUME_DOWN 22 // 35
+#define BUTTON_VOLUME_UP 23 // 27
 
-#define BUTTON_VOLUMEPRESSURE_UP 25
-#define BUTTON_VOLUMEPRESSURE_DOWN 33
+#define BUTTON_PRESSURE_DOWN 24 // 33
+#define BUTTON_PRESSURE_UP 25
 
-#define BUTTON_VOLUME_UP 27
-#define BUTTON_VOLUME_DOWN 35
+#define BUTTON_TRIG_DOWN 26 // 39
+#define BUTTON_TRIG_UP 27 // 31
 
-#define BUTTON_BPM_UP 29
-#define BUTTON_BPM_DOWN 37
+#define BUTTON_RR_DOWN 28 // 37
+#define BUTTON_RR_UP 29 // 29
 
-#define BUTTON_TRIG_UP 31
-#define BUTTON_TRIG_DOWN 39
+#define BUTTON_START_STOP 30 // 41
 
-#define BUTTON_START_STOP 41
+#define BUTTON_MUTE 31 // 45
 
-#define BUTTON_BREATH 43
+#define BUTTON_HOLD 32 // 43
 
-#define BUTTON_MUTE 45
+#define BUTTON_MODE 33 //23
 
+#define BUTTON_PRESSURE_ALARM_DOWN 34 // NEW
+#define BUTTON_PRESSURE_ALARM_UP 35 // NEW
 
+#define BUTTON_VOLUME_ALARM_DOWN 36 // NEW
+#define BUTTON_VOLUME_ALARM_UP 37 // NEW
 
-#define ALARMPIN 44
+#define BUTTON_PEEP_ALARM_DOWN 43 // NEW
+#define BUTTON_PEEP_ALARM_UP 44 // NEW
+
+#define BUTTON_IE_DOWN 45 // NEW
+#define BUTTON_IE_UP 46 // NEW
